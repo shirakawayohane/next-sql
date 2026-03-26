20af810f81bed1efd8797278c4049fd86205971b
shirakawayohane <mikuto.matsuo@icloud.com>
2026-03-26 17:51:11 +0900

    improve e2e

---
+++ examples/sample-ec-project/generated/src/lib.rs
@@ -0,0 +1,9 @@
+// nextsql:begin
+mod generated;
+pub use generated::types;
+pub use generated::analytics;
+pub use generated::customers;
+pub use generated::orders;
+pub use generated::products;
+pub use generated::reviews;
+// nextsql:end
